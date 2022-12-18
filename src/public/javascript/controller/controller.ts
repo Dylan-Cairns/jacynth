69255af3287658af5d3a5aeedb5241a36321a0e4
Dylan Cairns <dcairns@wgu.edu>
2022-12-17 19:38:43 -0700

    remove more multiplayer code

--- src/public/javascript/controller/controller.ts
+++ src/public/javascript/controller/controller.ts
@@ -1,12 +1,7 @@
-import {
-  Layout,
-  MultiplayerGameModel,
-  SinglePlayerGameModel
-} from '../model/model.js';
+import { Layout, SinglePlayerGameModel } from '../model/model.js';
 import { DeckType } from '../model/decktet.js';
 import { AIDifficulty, PlayerID } from '../model/player.js';
-import { MultiPlayerView, SinglePlayerView, View } from '../view/view.js';
-import { Socket } from 'socket.io-client';
+import { SinglePlayerView, View } from '../view/view.js';
 
 export class Controller {}
 
@@ -71,67 +66,4 @@ export class SinglePlayerController {
     this.model.startGame(layout, aiDifficulty);
     this.view.enableCardHandDragging();
   };
-}
-
-export class MultiPlayerController {
-  model: MultiplayerGameModel;
-  view: View;
-  currentPlayer: PlayerID;
-  socket: Socket;
-  constructor(deckType: DeckType, currentPlayer: PlayerID, socket: Socket) {
-    this.currentPlayer = currentPlayer;
-    this.socket = socket;
-    this.model = new MultiplayerGameModel(
-      deckType,
-      'multiPlayer',
-      socket,
-      currentPlayer
-    );
-
-    this.view = new MultiPlayerView(
-      this.model.board,
-      socket,
-      currentPlayer,
-      this.model.opposPlyr.playerID
-    );
-
-    this.model.currPlyr.bindDrawCard(this.view.playerDrawCardCB);
-    this.model.opposPlyr.bindSendCardPlayToView(
-      this.view.nonPlayerCardPlacementCB
-    );
-    this.model.opposPlyr.bindSendTokenPlayToView(
-      this.view.nonPlayerTokenPlacementCB
-    );
-    this.model.bindSendCardPlayToView(this.view.nonPlayerCardPlacementCB);
-
-    this.view.bindGetAvailCardSpaces(this.model.board.getAvailableSpaces);
-    this.view.bindGetAvailTokenSpaces(
-      this.model.currPlyr.getAvailableTokenSpaces
-    );
-    this.view.bindGetRemainingSpaces(this.model.board.getRemainingSpacesNumber);
-    this.view.bindSendCardPlayToModel(this.model.currPlyr.playCard);
-    this.view.bindSendTokenPlayToModel(this.model.currPlyr.placeToken);
-    this.view.bindUndoPlayCard(this.model.currPlyr.undoPlayCard);
-    this.view.bindUndoPlaceToken(this.model.currPlyr.undoPlaceToken);
-    this.view.bindGetCardDrawFromModel(this.model.currPlyr.drawCard);
-    this.view.bindGetCurrPlyrAvailTokens(
-      this.model.currPlyr.getInfluenceTokensNo
-    );
-    this.view.bindGetOpponAvailTokens(
-      this.model.opposPlyr.getInfluenceTokensNo
-    );
-    this.view.bindGetCurrPlyrScore(this.model.currPlyr.getScore);
-    this.view.bindGetOpponentScore(this.model.opposPlyr.getScore);
-
-    this.view.bindStartGame(this.model.createLayout);
-
-    this.view.bindGetControlledSpaces(
-      this.model.board.getSpacesControlledByToken
-    );
-
-    this.view.bindAddRecordtoDB(this.model.addRecordtoDB);
-
-    if (this.currentPlayer === 'Player 1')
-      socket.emit('playerReady', currentPlayer);
-  }
 }
